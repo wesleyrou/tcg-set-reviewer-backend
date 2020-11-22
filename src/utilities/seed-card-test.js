57030077ed0c72fc4b67024ca3c83f7c19e2a924
nickthorpe71 <nickthorpe71@gmail.com>
2020-11-21 22:48:31 -0600

    adds working json stream

--- src/utilities/seed-card-test.js
+++ src/utilities/seed-card-test.js
@@ -1,6 +1,31 @@
-const data = require('./small-cards.json');
+// let filePath = '../../../data/all-cards-large.json';
+let filePath = './small-cards.json';
+const StreamArray = require('stream-json/streamers/StreamArray');
+const fs = require('fs');
 
-console.log(data[0]);
+const jsonStream = StreamArray.withParser();
 
+//internal Node readable stream option, pipe to stream-json to convert it for us
+fs.createReadStream(filePath).pipe(jsonStream.input);
 
+let chunk = [];
+const chunkSize = 3;
 
+//You'll get json objects here
+//Key is the array-index here
+jsonStream.on('data', data => {
+  // add new d to array
+  chunk.push(data);
+
+  // when array length == chunkSize
+  if (chunk.length === chunkSize) {
+    // console log and clear array
+    console.log(chunk); // need to change console log to insert to db
+    chunk = [];
+  }
+});
+
+jsonStream.on('end', () => {
+  // console log remaining chunk
+  console.log(chunk); // need to change console log to insert to db
+});
