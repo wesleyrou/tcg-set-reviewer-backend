e9917d5f463a707530b44889a3826fe96269d3bd
Wesley Rou <wesleyrou@gmail.com>
2021-01-11 00:32:44 -0800

    x

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -9,7 +9,6 @@ reviewsRouter
     const { user_id } = req.body;
     const { setID } = req.params;
     const set_id = setID;
-<<<<<<< HEAD
     
     ReviewsService.getMatchingReviews(user_id, set_id, req.app.get('db'))
     .then(matchingReviews => {
@@ -26,29 +25,6 @@ reviewsRouter
             .catch(next)  
         
         //if user_id and set_id match a review then post nothing return the cardreviews with matching review_id
-=======
-
-
-    ReviewsService.getMatchingReviews(user_id, set_id, req.app.get('db'))
-      .then(matchingReviews => {
-        //if user_id and set_id match a review then post nothing return the cardreviews with matching review_id
-        if (matchingReviews.length === 0) {
-          const newReview = {
-            user_id,
-            set_id
-          };
-          ReviewsService.postReview(newReview, req.app.get('db'))
-            .then(newReview => {
-              ReviewsService.postNewCardReviews(newReview, req.app.get('db'))
-                .then(cardReviews => {
-                  return res.status(201).json(cardReviews);
-                })
-                .catch(next);
-            })
-            .catch(next);
-
-          //else post review with user_id and set_id && post empty cardreviews with matching review_id
->>>>>>> 0011a972e1164ece08a9601a10485066224af655
         } else {
           ReviewsService.getCardReviews(matchingReviews[0].id, req.app.get('db'))
             .then(cardReviews => {
