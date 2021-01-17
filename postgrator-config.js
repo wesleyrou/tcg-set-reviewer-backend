ca75df1f7da9c4b0f95972d6f8915977523c889d
Wesley Rou <wesleyrou@gmail.com>
2021-01-16 17:40:58 -0800

    ughhhhhhhhhh pg is killing me

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,5 @@ module.exports = {
   'migrationsDirectory': 'migrations',
   'driver': 'pg',
   'connectionString': process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: true,
 };
