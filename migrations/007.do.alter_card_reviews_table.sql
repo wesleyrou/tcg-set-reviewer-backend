d1800bb039608a1b809e34326eadfc0e7cc0318f
Wesley Rou <wesleyrou@gmail.com>
2021-01-18 23:16:17 -0800

    fixed migration 007

--- migrations/007.do.alter_card_reviews_table.sql
+++ migrations/007.do.alter_card_reviews_table.sql
@@ -1,4 +1,7 @@
 ALTER TABLE card_reviews
     DROP COLUMN rating
+;
+
+ALTER TABLE card_reviews
     ADD COLUMN rating NUMERIC(2,2)
 ;
---
+++ migrations/007.undo.alter_card_reviews_table.sql
@@ -0,0 +1,7 @@
+ALTER TABLE card_reviews
+    DROP COLUMN rating
+;
+
+ALTER TABLE card_reviews 
+    ADD rating INTEGER
+;
