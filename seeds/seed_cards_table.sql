263b45ad3a6020f4e94e7fdca27b84b1e7306455
Wesley Rou <wesleyrou@gmail.com>
2021-01-20 04:32:32 -0800

    wrote seed files to add missing users and cards

---
+++ seeds/seed_cards_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO cards (set_id, scryfall_id, card_name, lang, cost, card_type, color, rarity, image_url)
+VALUES
+  ('653', '34c599c7-bcc1-4005-b830-1fa4811af66e', 'Elven Bow', 'en', '{G}', 'Artifact — Equipment', 'G', 'uncommon', 'https://c1.scryfall.com/file/scryfall-cards/normal/front/3/4/34c599c7-bcc1-4005-b830-1fa4811af66e.jpg?1611084738'),
+  ('653', 'a67747a9-3299-4b4a-a4e0-e8a269c4f927', 'Struggle for Skemfar', 'en', '{3}{G}', 'Sorcery', 'G', 'common', 'https://c1.scryfall.com/file/scryfall-cards/normal/front/a/6/a67747a9-3299-4b4a-a4e0-e8a269c4f927.jpg?1611080386');
+
+
+
+  
---
+++ seeds/seed_users_table_w_new_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, password)
+VALUES
+  ('Jamie', '$2a$12$z0u7H3yrQTbfxhv55C0JleoQFXK5uvZ2wqKUqqV3NsOBcfZ9B5NOW');
