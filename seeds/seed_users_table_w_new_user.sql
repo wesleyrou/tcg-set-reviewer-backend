b4dc8533c37de4760da118543e00584552dd6a25
Wesley Rou <wesleyrou@gmail.com>
2021-03-26 16:43:57 -0700

    new user seed script added

--- seeds/seed_users_table_w_new_user.sql
+++ seeds/seed_users_table_w_new_user.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (username, password)
 VALUES
-  ('Jamie', '$2a$12$z0u7H3yrQTbfxhv55C0JleoQFXK5uvZ2wqKUqqV3NsOBcfZ9B5NOW');
+  ('Betts', '$2a$12$z0u7H3yrQTbfxhv55C0JleoQFXK5uvZ2wqKUqqV3NsOBcfZ9B5NOW');
